36194464ca6d413d8351950915bc906c47a453b0
naohaya <naohaya@cc.kyoto-su.ac.jp>
2024-02-28 14:22:29 +0900

    increasing loop counter in Client.java

--- src/com/client/Client.java
+++ src/com/client/Client.java
@@ -375,7 +375,7 @@ public class Client implements ClientInterface{
 		// TODO Auto-generated method stub
 		Client client = new Client();
 		client.initServer();
-		for(int i=0;i<4;i++){
+		for(int i=0;i<10;i++){
 			byte[] data = new byte[Configs.BLOCK_DATA_LEN];
 			Arrays.fill(data, (byte)i);
 			client.oblivious_access(i, OPERATION.ORAM_ACCESS_WRITE, data);
@@ -383,7 +383,7 @@ public class Client implements ClientInterface{
 		byte[] newdata = new byte[Configs.BLOCK_DATA_LEN];
 		Arrays.fill(newdata, (byte)12);
 		client.oblivious_access(3, OPERATION.ORAM_ACCESS_WRITE, newdata);
-		for(int i=0;i<4;i++){
+		for(int i=0;i<10;i++){
 			byte[] data = new byte[Configs.BLOCK_DATA_LEN];
 			//Arrays.fill(data, (byte)1);
 			data = client.oblivious_access(i, OPERATION.ORAM_ACCESS_READ, data);
