fe3d8d6c7e08f37ed97f3c6c3e084ce4d05824d8
naohaya <naohaya@cc.kyoto-su.ac.jp>
2024-02-28 16:46:37 +0900

    modified scripts.

--- bin/run_client.sh
+++ bin/run_client.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-if [ ! -d /tmp/serverStorage ]; then
-        mkdir /tmp/serverStorage 
-fi
 java -cp .:./lib/guava-33.0.0-jre.jar com.client.Client 
--- bin/run_server.sh
+++ bin/run_server.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+if [ ! -d /tmp/serverStorage ]; then
+        mkdir /tmp/serverStorage 
+fi
 java -cp .:./lib/guava-33.0.0-jre.jar com.server.Server &
