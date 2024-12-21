8e0168835b6a78c0b84aacc27496b90fbab35a58
川端一彰 <kawabatakazuaki@kawabatakazuakinoMacBook-Pro.local>
2024-12-21 18:13:46 +0900

    small update

--- bin/run_client.sh
+++ bin/run_client.sh
@@ -2,4 +2,4 @@
 if [ ! -d /tmp/serverStorage ]; then
       mkdir /tmp/serverStorage 
 fi
-java -cp .:./lib/guava-33.0.0-jre.jar com.client.Client 
+java -cp .:./lib/guava-33.0.0-jre.jar com.client.Client
--- bin/run_server.sh
+++ bin/run_server.sh
@@ -1,2 +1,2 @@
-#!/bin/sh
+#!/bin/sh 
 java -cp .:./lib/guava-33.0.0-jre.jar com.server.Server &
